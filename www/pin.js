d0a5a84e0089e6a3fceb8957ec7ac9aab0fcd853
MrLiZ <819493212@qq.com>
2017-10-11 00:39:28 -0500

    Update pin.js

--- www/pin.js
+++ www/pin.js
@@ -4,7 +4,7 @@ var exec = require('cordova/exec');
 module.exports = {
 
 	prompt: function(message, resultCallback, title, buttonLabels) {
-        var _message = (message || "Message");
+        var _message = (message || "");
         var _title = (title || "Title");
         var _buttonLabels = (buttonLabels || ["OK","Cancel"]);
         cordova.exec(resultCallback, null, "PinDialog", "prompt", [_message, _title, _buttonLabels]);
